5f67543515b55686c532732dcecbc60781e437d2
oscardenas17 <oscardenas17@gmail.com>
2022-08-07 19:16:40 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+function App() {
+
+
+  return (
+    <div className="App">
+     <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+    </div>
+  )
+}
+
+export default App
