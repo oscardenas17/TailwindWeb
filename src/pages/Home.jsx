923195adef754f56cc99be9bf8d559cad7f0ad17
oscardenas17 <oscardenas17@gmail.com>
2022-08-09 10:19:58 -0500

    Header separador- btn buy and watch con img fondo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,10 @@
 import React from 'react'
 
+import Header from '../components/Header';
+
 const Home = ({greeting}) => {
   return (
-    <>
-      <section>
-        <picture className='w-full'>
-         <img src="img/hero.jpg" alt="" className='w-full h-96'/>
-        </picture>
-
-        <section>
-          <div className="bg-wave-pattern h-6 bg-repeat-x relative -top-2">
-
-          </div>
-        </section>
-       
-      </section>
-    </>
+<Header />
   )
 }
 
